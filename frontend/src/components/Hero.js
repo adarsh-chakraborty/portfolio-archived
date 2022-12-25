04cc0970eb4d61edac81d825a6ac8e2ea885ebc8
Adarsh Chakraborty <adarshchak108@gmail.com>
2022-12-25 15:46:58 +0530

    added particlejs and amazon review scrapper project

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -1,19 +1,23 @@
 import React from 'react';
+import Particle from './Particle';
 
 const Hero = () => {
   return (
-    <section className="shadow-md h-72 bg-gradient-to-r from-yellow-200 via-green-200 to-green-300 flex flex-col justify-center items-center">
-      <div className="mt-4 text-3xl font-bold font-Poppins text-emerald-900 pb-2 border-b-2 border-green-900">
-        Adarsh Chakraborty
-      </div>
-      <div className="pt-1 text-xl font-Baloo text-emerald-900">
-        MERN Stack Developer
-      </div>
+    <>
+      <section className="z-10 shadow-md h-72 bg-gradient-to-r from-yellow-200 via-green-200 to-green-300 flex flex-col justify-center items-center">
+        <Particle />
+        <div className="z-10 mt-4 text-3xl font-bold font-Poppins text-emerald-900 pb-2 border-b-2 border-green-900">
+          Adarsh Chakraborty
+        </div>
+        <div className="z-10 pt-1 text-xl font-Baloo text-emerald-900">
+          MERN Stack Developer
+        </div>
 
-      <div className="mt-8 text-emerald-800 font-Cookie text-2xl">
-        "I code simple things and I love what i do..."
-      </div>
-    </section>
+        <div className="z-10 mt-8 text-emerald-800 font-Cookie text-2xl">
+          "I code simple things and I love what i do..."
+        </div>
+      </section>
+    </>
   );
 };
 
